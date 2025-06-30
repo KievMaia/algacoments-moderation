b4d5e4347e47c0a7ec543281d8383a1ee85db4d3
Kiev Da Silva Maia <kiev.maia@portoseguro.com.br>
2025-06-30 12:31:34 -0300

    New projcts files

---
+++ src/main/java/com/kiev/algamoderation/AlgamoderationApplication.java
@@ -0,0 +1,13 @@
+package com.kiev.algamoderation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgamoderationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgamoderationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiev/algamoderation/AlgamoderationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiev.algamoderation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgamoderationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
